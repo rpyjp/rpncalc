6192e82b060299449e9fd32c9fdc4705f2c0c3d9
rpy <rappy.contact@gmail.com>
2021-03-22 22:24:17 +0900

    read file

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use clap::Clap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "rpyjp",
+    about = "Super awesome sample RPN calculator"
+)]
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
+
+fn main() {
+    let opts = Opts::parse();
+
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+        run(reader, opts.verbose);
+    } else {
+        println!("No file is specified");
+    }
+}
+
+fn run(reader: BufReader<File>, verbose: bool) {
+    for line in reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
+    }
+}
